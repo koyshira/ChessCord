aa9e8b472510bffd1f6aa7dcb99934bf2fc91b7d
Dylano Zuijderwijk <dylano@familiezuijderwijk.nl>
2023-10-31 16:54:29 +0100

    Main files

---
+++ events/ready.js
@@ -0,0 +1,11 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Bot is now online. Logged in as ${client.user.tag}.`);
+		console.log(`Currently in ${client.guilds.cache.size} server(s).`);
+		console.log(`Total users: ${client.users.cache.size}`);
+	},
+};
---
+++ index.js
@@ -0,0 +1,62 @@
+// Require the necessary modules
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
+
+// Load environment variables from a .env file
+dotenv.config();
+
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+	presence: {
+		activities: [{ name: 'Your Chess game in Discord', type: 4 }],
+		status: 'dnd',
+	},
+	afk: false,
+	shards: [0],
+});
+
+client.commands = new Collection();
+
+// Read all the command files
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+		}
+	}
+}
+
+// Read all the event files
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs
+	.readdirSync(eventsPath)
+	.filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Log in to Discord with your client's token
+client.login(process.env.TOKEN);
